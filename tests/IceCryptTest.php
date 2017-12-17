5d931a315ab0f3e95d9539cd62eced1d836ad9ad
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-17 19:56:23 +0700

    Update test case

--- tests/IceCryptTest.php
+++ tests/IceCryptTest.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use IceCrypt\IceCrypt;
+use PHPUnit\Framework\TestCase;
 
-class IceCryptTest
+class IceCryptTest extends TestCase
 {
-	public function 
+	public function testBinarySafe()
+	{
+		for ($i=0; $i < 1000; $i++) { 
+			$string = self::randString($i);
+			$key	= self::randString(rand(32, 64));
+			$encryptedString = IceCrypt::encrypt($string, $key);
+			$this->assertEquals(
+				IceCrypt::decrypt($encryptedString, $key), $string
+			);
+		}
+	}
+
+	private static function randString($n)
+	{
+		$r = "";
+		for ($i=0; $i < $n; $i++) { 
+			$r .= chr(rand(1, 255));
+		}
+		return $r;
+	}
 }
